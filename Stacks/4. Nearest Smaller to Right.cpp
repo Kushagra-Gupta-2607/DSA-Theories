f260d23843b8453946229197c76cb001e19c4ff2
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-04-30 14:00:59 +0530

    Create 4. Nearest Smaller to Right.cpp

---
+++ Stacks/4. Nearest Smaller to Right.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nearest_smaller_int(vector<int>& v){
+    
+    int n = v.size();
+    vector<int> res;
+    stack<int> st;
+    
+    for(int i = n-1; i>-1; i--){
+        if(st.empty()) res.push_back(-1);
+        else{
+            if(st.top()>=v[i]){
+                st.pop();
+                i++;
+                continue;
+            }else{
+                res.push_back(st.top());
+            }
+        }
+        st.push(v[i]);
+    }
+    
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+int main(){
+    
+    int n;
+    cin >> n;
+    vector<int> v(n), ans;
+    for(int i = 0; i<n; i++) cin >> v[i];
+
+    ans = nearest_smaller_int(v);
+    for(int i = 0; i<n; i++) cout << ans[i] << " ";
+    
+    return 0;
+}
