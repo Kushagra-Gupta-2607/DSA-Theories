e7d3f12c5e854d76f3090706d86493f08406b358
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-05-08 15:05:39 +0530

    Update 1. Binary Search.cpp

--- Binary Search/1. Binary Search.cpp
+++ Binary Search/1. Binary Search.cpp
@@ -10,3 +10,16 @@ int binarySearch(vector<int>& arr, int target){
     return -1;
 
 }
+
+int binarySearch_ReverseSortedArray(vector<int>& arr, int target){
+    
+    int l = 0, r = arr.size()-1;
+    while(l<=r){
+        int mid = l + (r-l)/2;
+        if(arr[mid] == target) return mid;
+        else if(arr[mid]<target) r = mid-1;
+        else l = mid+1;
+    }
+    return -1;
+
+}
